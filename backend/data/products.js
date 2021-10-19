9b787769dc82e889418d842ff9ff518e1b8995c7
Brayan <brayan2191946@correo.uis.edu.co>
2021-10-18 22:25:14 -0500

    Se crea el proyecto final de web

---
+++ backend/data/products.js
@@ -0,0 +1,85 @@
+const products = [
+  {
+   name: "BURGUER V",
+    imageUrl:
+      "https://static-images.ifood.com.br/image/upload/t_medium/pratos/21c9a91f-2beb-4f54-9eb0-2328db2ce87e/202103011226_vjwG_.jpeg",
+    description:
+      "hamburguesa en forma de cono rellena de papa molida y pollo desmechado, tocineta , carne de 130g, lechuga tomate y cebolla fresca.",
+    price: 9900,
+    countInStock: 5,
+  },
+  {
+    name: "HAMBURGUESA LA TIGER´S",
+    imageUrl:
+      "https://static-images.ifood.com.br/image/upload/t_medium/pratos/d3b68038-4183-4808-9f02-5e4515ba3f44/202106032008_4CG8_.jpeg",
+    description:
+      "Doble carne, 300 gr de exquisita carne de res, 180 gr de filete de pollo, queso mozzarella, queso cheddar, triple tocineta ahumada, aros de cebolla, tomate, lechuga, pan parmesano, papas a la francesa y salsas",
+    price: 23000,
+    countInStock: 3,
+  },
+  {
+    name: "LA DEVASTADORA",
+    imageUrl:
+      "https://static-images.ifood.com.br/image/upload/t_medium/pratos/d3b68038-4183-4808-9f02-5e4515ba3f44/202103311527_tLGo_.jpeg",
+    description:
+      "160 gr de exquisita carne de res, queso mozzarella, queso cheddar, pollo desmechado bañado en salsa de la casa, deliciosos pedacitos de tocineta ahumada, chorizo al carbón, tomate, lechuga, papas a la francesa y salsas",
+    price: 18000,
+    countInStock: 7,
+  },
+  {
+    name: "HOT DOG MEGA SALVAJE(35CM)",
+    imageUrl:
+      "https://static-images.ifood.com.br/image/upload/t_medium/pratos/d3b68038-4183-4808-9f02-5e4515ba3f44/202005011829_vBhv_a.png",
+    description:
+      "35 cm. Salchicha premium, pan parmesano, papa molida, queso mozzarella doble gratinado, jamón de cerdo doble, pollo desmechado, tocineta, pedacitos de chorizo al carbón, huevo de codorniz y papas a la Francesa",
+    price: 20000,
+    countInStock: 13,
+  },
+  {
+    name: "HOT DOG DEPREDADOR (35CM)",
+    imageUrl:
+      "https://static-images.ifood.com.br/image/upload/t_medium/pratos/d3b68038-4183-4808-9f02-5e4515ba3f44/202005011829_jq2e_q.png",
+    description:
+      "35 cm. Salchicha premium, pan parmesano, papa molida, queso mozzarella doble gratinado, jamón de cerdo doble, pollo desmechado, tocineta y papas a la Francesa.",
+    price: 18000,
+    countInStock: 11,
+  },
+  {
+    name: "HOT DOG CACHORRO (35CM)",
+    imageUrl:
+      "https://static-images.ifood.com.br/image/upload/t_medium/pratos/d3b68038-4183-4808-9f02-5e4515ba3f44/202007291045_dCDI_.jpeg",
+    description:
+      "35 cm. Salchicha Americana, pan parmesano, queso mozzarella, jamón de cerdo, papa molida y papas a la Francesa",
+    price: 14900,
+    countInStock: 20,
+  },
+   {
+    name: "LA DESPIADADA!",
+    imageUrl:
+      "https://static-images.ifood.com.br/image/upload/t_medium/pratos/d3b68038-4183-4808-9f02-5e4515ba3f44/202005011832_yuME_l.png",
+    description:
+      "Papas a la Francesa triple tocineta ahumada, doble jamon de cerdo, pollo desmechado bañado en salsa de la casa, pedacitos de pechuga al carbon, Chorizo De Cerdo, salchicha americana, maiz Tierno, doble queso mozzarela, lechuga bañada en salsa y salsas",
+    price: 22900,
+    countInStock: 16,
+  },
+   {
+    name: "LA BESTIA",
+    imageUrl:
+      "https://static-images.ifood.com.br/image/upload/t_medium/pratos/d3b68038-4183-4808-9f02-5e4515ba3f44/202005011833_gmHH_t.png",
+    description:
+      "Papas a la francesa, delicioso Chorizo premiun al carbon, chorizo de cerdo doble queso mozzarella Fundido, Doble Jamón De Cerdo, salchicha americana, delicioso Pollo Desmechado bañado en salsa de la Casa, lechuga en salsa y salsas",
+    price: 18000,
+    countInStock: 12,
+  },
+    {
+    name: "SALCHIPAPA",
+    imageUrl:
+      "https://static-images.ifood.com.br/image/upload/t_medium/pratos/d3b68038-4183-4808-9f02-5e4515ba3f44/202110121837_B66H_i.jpg",
+    description:
+      "Papas a la francesa, salchicha americana en bbq, queso fundido, tartara y piña",
+    price: 11950,
+    countInStock: 30,
+  },
+];
+
+module.exports = products;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,80 @@
+import "./ProductScreen.css";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Actions
+import { getProductDetails } from "../redux/actions/productActions";
+import { addToCart } from "../redux/actions/cartActions";
+
+const ProductScreen = ({ match, history }) => {
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.getProductDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    if (product && match.params.id !== product._id) {
+      dispatch(getProductDetails(match.params.id));
+    }
+  }, [dispatch, match, product]);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart(product._id, qty));
+    history.push(`/cart`);
+  };
+
+  return (
+    <div className="productscreen">
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <>
+          <div className="productscreen__left">
+            <div className="left__image">
+              <img src={product.imageUrl} alt={product.name} />
+            </div>
+            <div className="left__info">
+              <p className="left__name">{product.name}</p>
+              <p>Precio: ${product.price}</p>
+              <p>Descripccion: {product.description}</p>
+            </div>
+          </div>
+          <div className="productscreen__right">
+            <div className="right__info">
+              <p>
+                Precio:
+                <span>${product.price}</span>
+              </p>
+              <p>
+                Estado:
+                <span>
+                  {product.countInStock > 0 ? "Disponible" : "No disponible"}
+                </span>
+              </p>
+              <p>
+                Cantidad
+                <select value={qty} onChange={(e) => setQty(e.target.value)}>
+                  {[...Array(product.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>
+                  ))}
+                </select>
+              </p>
+              <p>
+                <button type="button" onClick={addToCartHandler}>
+                  Añadir al carrito
+                </button>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ProductScreen;
